475c7f2bf5939c35e84d5c3654370273fd6e6182
tek707 <tekkwaku7@gmail.com>
2024-07-31 21:13:05 -0700

    question two of day one

---
+++ day_one.py
@@ -0,0 +1,26 @@
+# Question one  : Write a Python program to print "Hello, World!"
+
+# Answer
+
+print("Hello , World!")
+
+
+# Question two  : Calculate the sum of two numbers entered by the user.
+
+# Answer
+
+# we first declear two variables called numone and numtwo
+# We then prompt the user to enter the values of both numone and the numtwo variables
+# Generate a new variable call sum 
+# assign numone + numtwo to sum variable 
+# Print out sum
+
+numone = 0 
+numtwo = 0
+
+numone = int(input ("Please enter the first value: "))
+numtwo = int(input ("Please enter the second value: "))
+
+sum 
+sum = numone + numtwo
+print ("The sum is : ",sum)
