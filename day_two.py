a1a96992f849de8a23698db658c7adab14b5f576
tek707 <tekkwaku7@gmail.com>
2024-08-09 05:10:26 -0700

    daytwo_question 1 and 2

---
+++ day_two.py
@@ -0,0 +1,24 @@
+ #Create variables for storing a person's name, age, and  average test score.
+
+ #Answer:
+
+name = ""
+age = 0
+average_test_score = 0.00
+
+
+#Concatenate two strings and print the result.
+
+#Answers:
+
+firstname = "Abena"
+lastname = "Kawi"
+
+print(firstname , lastname)
+
+
+#Create a list of fruits and access elements using indexing.
+
+fruitlist = ['Apple' , 'Banana' , 'mango' , 'pear' , 'watermelon' , 'sugarcane']
+
+print(fruitlist[0])
